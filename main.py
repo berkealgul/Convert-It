b1b0348971e2dde2606ba632a7fccc93c303e025
ardaalgull <a.algul8624@gmail.com>
2022-02-15 00:32:59 +0300

    pydube küüphanesini çözmeye çalışıyorum

---
+++ main.py
@@ -0,0 +1,5 @@
+from pydub import AudioSegment
+
+path = "C:\Berke\Sound-Video-Converter\src\Heartbeat flatline.mp3"
+media_type = path[-4:0] 
+AudioSegment.from_file(path, format=media_type).export("demo.wav", format='wav')
