b63ad7e240c966a69d8c0ef6124828fcaf7311ce
berkealgul <berkealgul@gmail.com>
2022-02-17 04:20:45 +0300

    ui geldi gibi

--- src/main.py
+++
@@ -1,5 +0,0 @@
-from pydub import AudioSegment
-#ss
-path = "C:\Berke\Sound-Video-Converter\src\Heartbeat flatline.mp3"
-media_type = path[-4:0] 
-AudioSegment.from_file(path, format=media_type).export("demo.wav", format='wav')
