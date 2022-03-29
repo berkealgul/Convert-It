8ebf59fe0662c24f8abcf95b04419a86980d11e3
Berke Algül <a.algul8624@gmail.com>
2022-03-29 05:13:21 +0300

    s<cdsc

--- mainwindow.py
+++ mainwindow.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QApplication
 from PyQt5.QtCore import Qt, QSize, QMimeData
 from PyQt5.QtGui import QDrag
 from PyQt5.uic import loadUi
+from sympy import true
 
 from medieItem import MedieItem
 
@@ -27,9 +28,34 @@ class MainWindow(QMainWindow):
         self.itemScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.itemScrollArea.setWidgetResizable(True)
         self.itemScrollArea.setWidget(self.widget)
+        # self.itemScrollArea.installEventFilter(self) 
+        # self.itemScrollArea.setAcceptDrops(True)
 
         # signals
         self.pushButton.clicked.connect(self.addItem) 
+
+    # def eventFilter(self, o, e):
+    #     if (o.objectName() == "itemScrollArea"):
+    #         if e.type() == QtCore.QEvent.DragEnter:
+    #             self.itemDragEnterEvent(e)
+    #             return True
+    #         if e.type() == QtCore.QEvent.Drop:
+    #             self.itemDropEvent(e)
+    #             return True
+
+    #     return super().eventFilter(o, e)
+
+    # def itemDragEnterEvent(self, event):
+    #     print("drag")
+    #     if event.mimeData().hasUrls():
+    #         event.accept()
+    #     else:
+    #         event.ignore()
+    
+    # def itemDropEvent(self, event):
+    #     print("drop")
+    #     print(event.mimeData.urls().path())
+
 
     def dragEnterEvent(self, event):
         print("drag")
