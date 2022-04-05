dd997c649235b95191208d665c1e67082e92078a
Berke Algül <80629081+ardaalgull@users.noreply.github.com>
2022-04-05 15:28:52 +0300

    thread wip

--- widgets.py
+++ widgets.py
@@ -2,22 +2,26 @@ import os
 from turtle import width
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QMenu
+from PyQt5.QtWidgets import QWidget, QMenu, QAction
 from PyQt5.uic import loadUi
 
+from converter import Converter
 
 class MedieItem(QWidget):
     def __init__(self, main_window, mediaPath):
         super().__init__()
         loadUi('qt_assets/item.ui', self)
-
         self.path = os.path.normpath(mediaPath)
-        self.name = self.path.split(os.sep)[-1]
-
         self.main_window = main_window
+        self.processPath()
         self.init_ui()
-
+        self.converted = False
     
+    def processPath(self):
+        self.name = self.path.split(os.sep)[-1]
+        self.format = None
+        self.targetFormat = None
+
     def init_ui(self):
         self.setMinimumHeight(self.geometry().height())   
         self.setMinimumWidth(self.geometry().width())   
@@ -26,13 +30,19 @@ class MedieItem(QWidget):
 
         #signals
         self.deleteButton.clicked.connect(self.deleteLater) 
+    
+    def setTargetFormat(self, targetFormat):
+        self.targetFormat = targetFormat
+        print(self.targetFormat)
 
 
 class FormatMenu(QMenu):
-    def __init__(self):
+    def __init__(self, mainWindow):
         super().__init__()
+        self.mainWindow = mainWindow
         data = {"Audio":["mp3", "wav"], "image":["png", "jpeg"]}
         self.setup_menu(data, self)
+        self.triggered[QAction].connect(mainWindow.setTargetFormat)
 
     def setup_menu(self, data, menu_obj):
         if isinstance(data, dict):
@@ -46,3 +56,11 @@ class FormatMenu(QMenu):
         else:
             action = menu_obj.addAction(data)
             action.setIconVisibleInMenu(False)
+
+
+class Worker(QObject):
+    def __init__(self, mediaItem):
+        super().__init__()
+        self.mediaItem = mediaItem
+    def doWork():
+        Converter.convert()
