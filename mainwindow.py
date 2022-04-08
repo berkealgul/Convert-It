bcf7a1bb0e631ff1542fdc439d15dd9bb4766691
Berke Algül <berkealgul@gmail.com>
2022-04-09 01:34:27 +0300

    icons

--- mainwindow.py
+++ mainwindow.py
@@ -7,21 +7,19 @@ from PyQt5.uic import loadUi
 from widgets import *
 
 
+# icons by <aS target="_blank" href="https://icons8.com/icon/KPhFC2OwpbWV/delete">Delete</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi('qt_assets/mainwindow.ui', self)
-        self.converterThread = self.createConverterThread()
+        self.n_convertedItems = 0
         self.setup_ui() 
-
-        # test case 
-        self.TEST_CASE()
-
+        self.TEST_CASE() # test case 
         self.show()
         
     def setup_ui(self):
         self.setAcceptDrops(True)
-
         # setting up scroll bar
         self.widget = QWidget()
         self.vbox = QVBoxLayout()
@@ -30,20 +28,24 @@ class MainWindow(QMainWindow):
         self.itemScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.itemScrollArea.setWidgetResizable(True)
         self.itemScrollArea.setWidget(self.widget)
-        
+        # format menu
         self.formatButton.setMenu(FormatMenu(self))
+        # thread
+        self.converterThread = ConverterThread(self)
 
         # signals
         self.convertButton.clicked.connect(self.conversionState)
+        self.converterThread.finished.connect(self.pickItemToConvert)
+        self.converterThread.finished.connect(self.updateProgressBar)
+        self.cancelButton.clicked.connect(self.abortConversion)
 
     def addItem(self, path):
         widget = MediaItem(self, path)
         self.vbox.addWidget(widget)    
 
-    def createConverterThread(self):
-        thread = ConverterThread(self)
-        thread.finished.connect(self.pickItemToConvert)
-        return thread
+    def updateProgressBar(self):
+        self.n_convertedItems = self.n_convertedItems+1
+        self.conversionProgressBar.setValue(self.n_convertedItems)
 
     def pickItemToConvert(self):
         for i in range(self.vbox.count()):
@@ -59,19 +61,27 @@ class MainWindow(QMainWindow):
         
         # if couldnt find anything we reset the state
         pass # for now
-            
+
+    def abortConversion(self):
+        self.converterThread.quit()
+        self.idleState()
+
     # program has two states
     # one is "idle" state user can interact with app normally 
     # on the other hand other state is "conversion" step,
     # this state activates after user starts the conversion process
-    # during this state some changes are on ui expected
+    # during this state some changes on ui is expected
     def conversionState(self):
+        self.convertButton.setEnabled(False)
+        self.conversionProgressBar.setMaximum(self.vbox.count())
         self.pickItemToConvert()
     
     # this reverts every changes conversion state does
     def idleState(self):
-        pass
-    
+        self.convertButton.setEnabled(True)
+        self.conversionProgressBar.setValue(0)
+        self.n_convertedItems = 0
+        
     def setTargetFormat(self, targetFormatAction):
         self.setTargetFormatByText(targetFormatAction.text())
     
@@ -108,6 +118,7 @@ class MainWindow(QMainWindow):
         self.addItem("C:\Berke\Portal 2 OST\Portal2-06-Overgrowth.mp3")
         self.setTargetFormatByText("wav")
            
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
